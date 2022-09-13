a81fe061943657dc5bbdec90aa63433664904049
MrMoussie <maouheb.bessi@gmail.com>
2022-09-13 13:15:52 +0200

    Fix dependencies

--- app/src/main/java/com/example/localization/ExcelReader.java
+++ app/src/main/java/com/example/localization/ExcelReader.java
@@ -1,7 +1,6 @@
 package com.example.localization;
 
 import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.CellType;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
